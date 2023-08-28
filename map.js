e96d9219f192c43637f989a2bbc4fe67763a0293
Saleha Mohammadi <saleha.moh2023@gmail.com>
2023-08-28 09:34:37 +0000

    Innitial commit

--- map.js
+++ map.js
@@ -4,16 +4,35 @@
  * command node map.js in the terminal
  */
 
-// Using a for loop
+/* Using a for loop
+let results = [];
+let nums = [1, 2, 3, 4, 5, 6];
+for(let num of nums){
+  results.push(num * 2);
+}
+console.log(results);
+*/
+
 
 
 // Using map()
+let nums = [1, 2, 3, 4, 5, 6];
+const multiplyByTwo = function(num) {
+  return num * 2;
+}
+const mapResults = nums.map(multiplyByTwo);
+console.log(mapResults);
+
 
 
 // Simplified w/ map()
+const simplifiedMap = nums.map(function (num) {return num * 2});
+console.log(simplifiedMap); 
 
 
 // Simplfied w/ map() + arrow function
+const simplifiedMapArrow = nums.map(num => num * 2);
+console.log(simplifiedMapArrow);
 
 
 // With objects:
@@ -37,3 +56,7 @@ const students = [
     skill: 'CSS'
   },
 ];
+students[0].push(age: 21);
+console.log(students[0]);
+const studentsWithIds = students.map(student => [students.name, students.id]);
+console.log(studentsWithIds);
